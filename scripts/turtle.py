7a122573c7e6744fadabed797a5cd3ff29df7501
Michał Szpunar <mszpuna1@gepard.robotyka.ia.pw.edu.pl>
2019-03-18 13:39:54 +0100

    commit2

--- scripts/turtle.py
+++ scripts/turtle.py
@@ -3,19 +3,20 @@ import click
 import rospy
 from geometry_msgs.msg import Twist 
 
-
 pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 rospy.init_node('turtle')
+
 while not rospy.is_shutdown():
-	print("1")
-	msg = Twist()
-	key = click.getchar()
-	if key == 'w':
-		msg.linear.x = 1
-	elif key == 'a':
-		msg.angular.z = 1
-	elif key == 's':
-		msg.linear.x = -1
-	elif key == 'd':
-		msg.angular.z = -1
-pub.publish(msg)
+	twist = Twist() #this expresses velocity in free space broken into its linear and angular parts.
+	key_pressed = click.getchar()
+
+	if key_pressed == '5':
+		twist.linear.x = 1.5
+	elif key_pressed == '1':
+		twist.angular.z = 3.5
+	elif key_pressed == '2':
+		twist.linear.x = -0.6
+	elif key_pressed == '3':
+		twist.angular.z = -4.20
+
+	pub.publish(twist)
