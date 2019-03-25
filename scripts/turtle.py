adfebf8a4d250ec60cb2f383eb3053e8a6d813f5
Michal Szpunar <szpunar98@gmail.com>
2019-03-25 21:10:24 +0100

    Now it loads parameters from the config file

--- scripts/turtle.py
+++ scripts/turtle.py
@@ -9,14 +9,18 @@ rospy.init_node('turtle')
 while not rospy.is_shutdown():
 	twist = Twist() #this expresses velocity in free space broken into its linear and angular parts.
 	key_pressed = click.getchar()
+	up = rospy.get_param("up")
+	down = rospy.get_param("down")
+	left = rospy.get_param("left")
+	right = rospy.get_param("right")
 
-	if key_pressed == '5':
+	if key_pressed == up:
 		twist.linear.x = 1.5
-	elif key_pressed == '1':
+	elif key_pressed == left:
 		twist.angular.z = 3.5
-	elif key_pressed == '2':
+	elif key_pressed == down:
 		twist.linear.x = -0.6
-	elif key_pressed == '3':
+	elif key_pressed == right:
 		twist.angular.z = -4.20
 
 	pub.publish(twist)
